cd5871defa1cc3f3b271da003cdb922c0c31db14
Miguel Angelo Fernández Iglesias <82086544+mfernandezig@users.noreply.github.com>
2025-01-09 22:38:05 +0100

    block clicks in view to synchronize with playwright

--- src/main/java/comdirect/controllers/MainController.java
+++ src/main/java/comdirect/controllers/MainController.java
@@ -172,7 +172,14 @@ public class MainController {
     }
 
     private void displayHtmlInWebView(String htmlContent) {
-        Platform.runLater(() -> webView.getEngine().loadContent(htmlContent, "text/html"));
+//        Platform.runLater(() -> webView.getEngine().loadContent(htmlContent, "text/html"));
+
+        Platform.runLater(() -> webView.getEngine().loadContent(
+                htmlContent + "<script>" +
+                        "document.querySelectorAll('a').forEach(link => link.addEventListener('click', e => e.preventDefault()));" +
+                        "document.querySelectorAll('form').forEach(form => form.addEventListener('submit', e => e.preventDefault()));" +
+                        "</script>"
+        ));
     }
 
     private void showError(String title, String header, String content) {
